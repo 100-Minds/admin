c06e029b3c855bb0ff26e4368feae017712c756c
Dave <uchennadavid2404@gmail.com>
2025-03-21 23:08:59 +0100

    Enable React strict mode and add experimental scroll restoration; update logo import path in LogoBanner component

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: 'export',
+	reactStrictMode: true,
+	experimental: {
+		scrollRestoration: true,
+	},
 };
 
 export default nextConfig;
--- src/components/common/LogoBanner.tsx
+++ src/components/common/LogoBanner.tsx
@@ -1,11 +1,11 @@
-import logo from '../../../public/100minds.jpg';
+//import logo from '../../../public/100minds.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const LogoBanner = ({ className }: { className?: string }) => {
 	return (
 		<Link href="/" className={`flex items-center justify-center gap-2`}>
-			<Image className="aspect-square w-6 md:w-10" src={logo} priority alt="logo" />
+			<Image className="aspect-square w-6 md:w-10" src='/public/100minds.jpg' priority alt="logo" />
 			<span role="" className={`font-bold md:text-xl ${className}`}>
 				100Minds
 			</span>
