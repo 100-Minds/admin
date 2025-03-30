19c0422b6fea3d2a0c103a0ae28485680720c3f0
Dave <uchennadavid2404@gmail.com>
2025-03-30 22:11:05 +0100

    Update page metadata image handling to use a default image

--- src/components/common/PageMetaData.tsx
+++ src/components/common/PageMetaData.tsx
@@ -20,31 +20,34 @@ export function generatePageMetadata({ title, content, image, url }: MetaDataPro
 			title: `${title}`,
 			description: content,
 			url: url || 'https://admin-mmyv.onrender.com',
-			images: [
-				{
-					url: image || defaultImage,
-					width: 1200,
-					height: 630,
-					alt: `${title} - 100 Minds`,
-				},
-			],
+			// images: [
+			// 	{
+			// 		url: image || defaultImage,
+			// 		width: 1200,
+			// 		height: 630,
+			// 		alt: `${title} - 100 Minds`,
+			// 	},
+			// ],
+			images: ['100minds.jpg']
 		},
 		twitter: {
 			card: image || defaultImage ? 'summary_large_image' : 'summary',
 			title: `${title}`,
 			description: content,
-			images: [
-				{
-					url: image || defaultImage,
-					width: 1200,
-					height: 630,
-					alt: `${title} - 100 Minds`,
-				},
-			],
+			// images: [
+			// 	{
+			// 		url: image || defaultImage,
+			// 		width: 1200,
+			// 		height: 630,
+			// 		alt: `${title} - 100 Minds`,
+			// 	},
+			// ],
+			images: ['100minds.jpg'],
 		},
 		other: {
 			'og:locale': 'en_US',
 		},
+		metadataBase: new URL('https://admin-mmyv.onrender.com'),
 	};
 }
 
