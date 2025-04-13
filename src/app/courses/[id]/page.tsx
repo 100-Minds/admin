4fc021670b07198af8138ff7ff9cebf1af821aa7
Dave <uchennadavid2404@gmail.com>
2025-04-13 01:34:04 +0100

    params edit

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -3,16 +3,18 @@ import { generatePageMetadata } from '@/components/common/PageMetaData';
 import CourseEditForm from '@/components/dashboard/CourseEditForm';
 import { Metadata } from 'next';
 
-export const generateMetadata = ({ params }: { params: { id: string } }): Metadata => {
-	return generatePageMetadata({
+export const generateMetadata = async (props: { params: Promise<{ id: string }> }): Promise<Metadata> => {
+    const params = await props.params;
+    return generatePageMetadata({
 		title: `Edit Course - 100 Minds`,
 		content: `Edit course details with 100 Minds Dashboard`,
 		url: `https://admin-mmyv.onrender.com/courses/${params.id}`,
 	});
 };
 
-export default function EditCourse({ params }: { params: { id: string } }) {
-	return (
+export default async function EditCourse(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params;
+    return (
 		<DashboardLayout heading="Edit Course">
 			<div className="mb-5">
 				<div className="flex items-center justify-center">
